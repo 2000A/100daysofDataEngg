d9795d1a33ba867772d89371a2083afb9f01ecd5
Abhishek Ram <66197106+2000A@users.noreply.github.com>
2025-10-21 21:10:26 +0530

    part 1 of joins using customers and orders table inside MyDatabaseDB

---
+++ 05_JOINs.sql
@@ -0,0 +1,244 @@
+/* ==============================================================================
+   SQL Joins 
+-------------------------------------------------------------------------------
+   This document provides an overview of SQL joins, which allow combining data
+   from multiple tables to retrieve meaningful insights.
+
+   Table of Contents:
+     1. Basic Joins
+        - INNER JOIN
+        - LEFT JOIN
+        - RIGHT JOIN
+        - FULL JOIN
+     2. Advanced Joins
+        - LEFT ANTI JOIN
+        - RIGHT ANTI JOIN
+        - ALTERNATIVE INNER JOIN
+        - FULL ANTI JOIN
+        - CROSS JOIN
+     3. Multiple Table Joins (4 Tables)
+=================================================================================
+*/
+
+
+/* ============================================================================== 
+   BASIC JOINS 
+=============================================================================== */
+
+--No Join
+/* Retrieve all data from customers and orders as separate resutls */
+
+SELECT * FROM customers;
+SELECT * FROM orders;
+
+/* INNER JOIN
+ Get all customers along with their orders,
+ but only for customers who have placed an order */
+
+SELECT 
+c.id,
+c.first_name,
+o.order_id,
+o.sales
+FROM customers AS c
+INNER JOIN orders AS o
+ON c.id = o.customer_id
+
+-- LEFT JOIN
+/* Get all customers along with their orders,
+	including those without orders */
+
+SELECT 
+	c.id,
+	c.first_name,
+	o.order_id,
+	o.sales
+FROM customers AS c
+LEFT JOIN orders AS o
+ON c.id = o.customer_id
+
+
+--Right JOIN
+/* Get all customers along with their orders,
+	indlcuding orders without matching customers*/
+
+SELECT 
+	c.id,
+	c.first_name,
+	o.order_id,
+	o.customer_id,
+	o.sales
+FROM customers AS c
+RIGHT JOIN orders AS o
+ON c.id = o.customer_id
+
+
+--Alternative to RIGHT JOIN using LEFT JOIN
+
+/*Get all customers along with their orders,
+	inluding orders without matching customers. */
+
+SELECT 
+	c.id,
+	c.first_name,
+	o.order_id,
+	o.customer_id,
+	o.sales
+FROM orders AS o
+LEFT JOIN customers AS c
+ON c.id = o.customer_id
+
+--FULL JOIN
+/* Get all customers and all orders, even if there's no match */
+
+SELECT 
+	c.id,
+	c.first_name,
+	o.order_id,
+	o.customer_id,
+	o.sales
+FROM customers AS c
+FULL JOIN orders AS o
+ON c.id = o.customer_id
+
+/* ============================================================================== 
+   ADVANCED JOINS
+=============================================================================== */
+
+--LEFT ANTI JOIN
+/* Get all customers who haven't placed any order */
+
+SELECT  
+	c.id,
+	c.first_name,
+	o.order_id,
+	o.customer_id,
+	o.sales
+FROM customers AS c
+LEFT JOIN orders AS o
+ON c.id = o.customer_id
+WHERE o.customer_id IS NULL
+
+-- RIGHT ANTI JOIN
+/* Get all orders without matching customers */
+
+SELECT  
+	c.id,
+	c.first_name,
+	o.order_id,
+	o.customer_id,
+	o.sales
+FROM customers AS c
+RIGHT JOIN orders AS o
+ON c.id = o.customer_id
+WHERE c.id IS NULL
+
+--Alternative to RIGHT ANTI JOIN usig LEFT JOIN
+/* Get all orders without matching customers */
+
+SELECT * 
+FROM orders AS o
+LEFT JOIN customers AS c
+ON c.id = o.customer_id
+WHERE c.id IS NULL
+
+
+-- Alternative to INNER JOIN using LEFT JOIN 
+/* Get all customers along with their orders,
+	but only for customers who have placed an order */
+
+SELECT *
+FROM customers AS c
+LEFT JOIN orders AS o
+ON c.id = o.customer_id
+WHERE o.customer_id IS NOT NULL
+
+-- FULL ANTI JOIN
+/* Find customers without orders and orders without customers */
+
+SELECT * 
+FROM customers AS c
+FULL JOIN orders AS o
+ON c.id = o.customer_id 
+WHERE c.id IS NULL OR o.customer_id IS NULL
+
+--CROSS JOIN 
+/* Generate all possible combinations of customers and orders */
+SELECT * 
+FROM customers
+CROSS JOIN orders
+
+/* ============================================================================== 
+   MULTIPLE TABLE JOINS (4 Tables)
+=============================================================================== */
+
+/* Task: Using SalesDB, Retrieve a list of all orders, along with the related customer, product, 
+   and employee details. For each order, display:
+   - Order ID
+   - Customer's name
+   - Product name
+   - Sales amount
+   - Product price
+   - Salesperson's name */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
